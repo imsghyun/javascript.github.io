67e58f504890810197418a1ceef46c4c18741fea
imsghyun <imsghyun@gmail.com>
2025-04-17 18:47:29 +0900

    up

---
+++ 1_basics/3_declaring_variable.js
@@ -0,0 +1,42 @@
+/**
+ *  Variable 선언하기
+ * 
+ * 1) var - 더이상 쓰지 않는다.
+ * 2) let
+ * 3) const
+ */
+var name = '코드팩토리'; // 따옴표를 쓰면 글자 안 쓰면 변수
+console.log(name);
+
+var age = 22;
+console.log(age);
+
+let band = '밴드';
+console.log(band);
+
+/**
+ * let과 var로 선언하면
+ * 값을 추후 변경할 수 있다.
+ */
+
+band = 'ㅊㅈㅇ';
+console.log(band);
+
+const newJeans = '뉴진스';
+console.log(newJeans);
+
+// newJeans = '코드팩토리'; >>const는 값을 추후 변경할 수 X
+
+/**
+ * 선언과 할당
+ * 
+ * 1) 변수를 선언하는 것.
+ * 2) 할당
+ */
+var name='코드팩토리';
+console.log(name);
+
+let dd;
+console.log(dd);
+
+const dd2;
---
+++ 1_basics/4_naming_conventions.js
@@ -0,0 +1,17 @@
+/**
+ * Naming Conventions
+ * 
+ * 1) 일반적으로 영어(라틴 문자)를 사용하며 문자와 숫자를 모두 사용 할 수 있다.
+ * 2) 특수기호는 언더스코어와 달러를 사용 할 수 있다.
+ * 3) 숫자로 이름을 시작 할 수 없다.
+ *     1Name, 2Hello
+ * 4) 키워드는 변수명으로 사용 할 수 없다.
+ *     var const = 'var';
+ */
+let name = '승현';
+var $ance = 'dance';
+const _hyun = '임승현'
+
+console.log(name);
+console.log($ance);
+console.log(_hyun);
