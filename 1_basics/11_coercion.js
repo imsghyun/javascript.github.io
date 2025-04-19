f32c4d174790b0fc080e5d9e7129e79334157f63
imsghyun <imsghyun@gmail.com>
2025-04-19 15:02:33 +0900

    up

--- 1_basics/11_coercion.js
+++ 1_basics/11_coercion.js
@@ -10,3 +10,24 @@ let age = 22;
 // 명시적
 let stringAge = age.toString();
 console.log(typeof stringAge, stringAge);
+
+// 암묵적
+let test = age + '';
+console.log(typeof test, test);
+
+console.log('98' + '2');
+// console.log('98' + 2);
+console.log('98' * 2);
+console.log('98' - 2);
+// 기능적으로는 가능하나 사용하지 마세요요
+console.log('------------');
+
+/**
+ * 명시적 변환 몇가지 더 배우기
+ */
+console.log(typeof (99).toString(), (99).toString());
+console.log(typeof (true).toString(), (true).toString());
+console.log(typeof (Infinity).toString(), (Infinity).toString());
+
+// 숫자 타입으로 변환
+console.log(typeof parseInt('0'), parseInt('0'));
