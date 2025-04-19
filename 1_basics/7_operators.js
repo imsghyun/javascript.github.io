7e3d7ca1f7e584c176ee058bf3be5c57d9017b27
imsghyun <imsghyun@gmail.com>
2025-04-19 13:37:45 +0900

    up

--- 1_basics/7_operators.js
+++ 1_basics/7_operators.js
@@ -107,4 +107,98 @@ console.log(number);
  * 2) 값의 타입의 비교
  */
 
-console
+console.log( 5 == 5);
+console.log(5 == '5');
+console.log(0 == '');
+console.log( true == 1);
+console.log( false == 0);
+console.log( true == '1');
+
+console.log( 5 === 5);
+console.log(5 === '5');
+console.log( true === 1);
+console.log( false === 0);
+console.log( true === '1');
+
+console.log('--------------');
+
+console.log( 5 != 5);
+console.log(5 != '5');
+console.log(0 != '');
+console.log( true!= 1);
+console.log( false != 0);
+console.log( true != '1');
+
+console.log('--------------');
+
+console.log( 5 !== 5);
+console.log(5 !== '5');
+console.log(0 !== '');
+console.log( true!== 1);
+console.log( false !== 0);
+console.log( true !== '1');
+
+console.log('--------------');
+/**
+ * 대소 관계 비교 연산자자
+ */
+console.log(100 > 1);
+console.log(100 < 1);
+console.log(100 <= 1);
+console.log(100 >= 1);
+
+/**
+ * 삼항 조건 연산자 (ternary operator)
+ */
+console.log(10 > 0 ? '10이 0보다 크다' : '10이 0보다 작다');
+
+console.log('--------------');
+
+/**
+ * 논리 연산자
+ * 
+ * 1) && 
+ * 2) ||
+ */
+
+// && 조건은 모두 true여야 true를 반환한다.
+console.log(true && true);
+console.log(true && false);
+console.log(false && true);
+console.log(false && false);
+
+console.log('--------------');
+
+// ||는 하나만 true여도 true를 반환한다.
+console.log(true || true);
+console.log(true || false);
+console.log(false || true);
+console.log(false || false);
+console.log('--------------');
+
+console.log(10 > 1 && 20 > 2);
+console.log(10 < 1 && 20 > 2);
+console.log(10 < 1 && 20 > 2);
+
+console.log(10 > 1 || 20 > 2);
+console.log(10 < 1 || 20 > 2);
+console.log(10 < 1 || 20 > 2);
+
+
+/**
+ * 단축평가 (shor circuit evaluation)
+ * 
+ * &&를 사용했을 때 좌측이 true면 우측 값 변환
+ * &&를 사용했을 때 좌측이 false면 좌측 값 변환
+ * ||를 사용했을 때 좌측이 true면 우측 값 변환
+ * ||를 사용했을 때 좌측이 false면 좌측 값 변환
+ */
+console.log('--------------');
+
+console.log(true || '학생');
+console.log(false || '학생');
+console.log(false && '학생');
+console.log(true && '학생');
+
+console.log(true && true && '학생');
+console.log(true && false && '학생');
