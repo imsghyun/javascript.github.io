3e38a83619fbb92c3c5c08c383cc132000580d39
imsghyun <imsghyun@gmail.com>
2025-04-19 14:55:49 +0900

    up

--- 1_basics/10_loops.js
+++ 1_basics/10_loops.js
@@ -4,4 +4,130 @@
  * 1) for
  * 2) while
  */
-for(let i = 0; i< 10; i ++)
+for(let i = 0; i < 10; i ++){
+    console.log(i);
+}
+
+console.log('-------------')
+
+for(let i = 10; i>0; i --){
+    console.log(i);
+}
+
+console.log('-------------')
+
+for(let i = 0; i<3; i ++){
+    for(let j = 3; j > 0; j--){
+        console.log(i,j);
+    }
+}
+
+// *을 이용해서 6x6의 정사각형을 출력해라
+let square ='';
+let side = 6;
+
+for( let i =0; i < 6; i++){
+    for(let j = 0; j < side; j++){
+        square += '*';
+    }
+    square += '\n';
+}
+
+console.log(square);
+
+/**
+ * for...in
+ */
+const seungHyun = {
+    name:'임승현',
+    year:'2004',
+    group:'학생',
+}
+
+console.log('--------');
+for(let key in seungHyun){
+    console.log(key);
+}
+
+const studentMembersArray = ['a','b','c','d','e','f'];
+
+console.log('--------');
+
+for(let key in studentMembersArray){
+    console.log(key);;
+    console.log(`${key}:${studentMembersArray[key]}`);
+}
+
+/**
+ * for...of
+ */
+for(let value of studentMembersArray){
+    console.log(value);
+}
+
+/**
+ * While
+ */
+let number = 0;
+
+while(number < 10){
+    number ++;
+}
+
+console.log(number);
+
+/**
+ * do...while(안쓰는걸추천)
+ */
+number = 0;
+
+do{
+    number++;
+}while(number < 10);
+
+console.log(number);
+
+/**
+ * break
+ */
+console.log('-------------');
+for(let i = 0; i<10; i++){
+    if(i === 5){
+        break;
+    }
+    console.log(i);
+}
+console.log('-------------');
+number = 0;
+
+while(number < 10){
+    if(number === 5){
+        break;
+    }
+    number ++;
+    console.log(number);
+}
+
+
+/**
+ * continue
+ */
+console.log('----------');
+for(let i = 0; i < 10; i++){
+    if(i === 5){
+        continue;
+    }
+    console.log(i);
+}
+
+console.log('----------');
+number = 0;
+
+while(number < 10){
+    number ++;
+
+    if(number === 5){
+        continue;
+    }
+    console.log(number);
+}
---
+++ 1_basics/11_coercion.js
@@ -0,0 +1,12 @@
+/**
+ * 타입 전환
+ * Type Conversion
+ * 
+ * 1) 명시적
+ * 2) 암묵적
+ */
+let age = 22;
+
+// 명시적
+let stringAge = age.toString();
+console.log(typeof stringAge, stringAge);
