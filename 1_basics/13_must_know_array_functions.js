df413139df68ae4e1c563133826d9fb28298235e
imsghyun <imsghyun@gmail.com>
2025-04-24 14:31:49 +0900

    up

--- 1_basics/13_must_know_array_functions.js
+++ 1_basics/13_must_know_array_functions.js
@@ -13,4 +13,64 @@ let iveMembers = [
 console.log(iveMembers);
 
 // push()
-iveMembers.push();   
+console.log(iveMembers.push('승현'));
+console.log(iveMembers);   
+
+console.log('--------------');
+// pop()
+console.log(iveMembers.pop());
+console.log(iveMembers);
+
+console.log('------------------------')
+// shift
+console.log(iveMembers.shift());
+console.log(iveMembers);
+
+// unshift()
+console.log(iveMembers.unshift('안유진'));
+console.log(iveMembers);
+
+console.log('--------------');
+
+console.log(iveMembers.splice(0, 3))
+console.log(iveMembers);
+
+iveMembers = [
+    '안유진',
+    '가을',
+    '레이',
+    '장원영',
+    '리즈',
+    '이서',
+];
+
+console.log(iveMembers);
+
+// concat()
+console.log(iveMembers.concat('승현'));
+console.log(iveMembers);
+
+//slice()
+console.log(iveMembers.slice(0,3));
+console.log(iveMembers);
+
+// spread operator
+let iveMembers2 = [
+    ...iveMembers,
+];
+console.log(iveMembers2);
+
+let iveMembers3 = [
+    iveMembers,
+];
+console.log(iveMembers3);
+
+console.log('-------------');
+let iveMembers4 = iveMembers;
+
+console.log(iveMembers4);
+console.log(iveMembers4 === iveMembers);
+
+console.log([
+    ...iveMembers,
+] === iveMembers);
