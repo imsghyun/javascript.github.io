cea3cff99ebabbb3cc2c724c099b1a22ed14f7f2
imsghyun <imsghyun@gmail.com>
2025-04-26 21:30:13 +0900

    up

---
+++ 1_basics/14_object.js
@@ -0,0 +1,15 @@
+/**
+ * Object /객체
+ */
+
+// key : value pair
+let seungHyun= {
+    name : '임승현',
+    group: '학생',
+    study : function(){
+        return'임승현이 공부를 합니다.'
+    }
+};
+console.log(seungHyun);
+console.log(seungHyun.name);
+console.log(seungHyun['name']);
